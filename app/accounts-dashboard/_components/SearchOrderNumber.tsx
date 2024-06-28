6ddab3a65a2a8a4ac4d9ca2bc1f706ce31f3d850
Jay Mehta <23277339+jaymehta20@users.noreply.github.com>
2024-06-28 14:15:59 +0530

    ui changs

--- app/accounts-dashboard/_components/SearchOrderNumber.tsx
+++ app/accounts-dashboard/_components/SearchOrderNumber.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
 import { useSearchParams } from "next/navigation";
@@ -8,43 +9,52 @@ const SearchOrderNumber = () => {
 	const orderNumber = searchParams.get("searchOrder");
 
 	return (
-		<div className="grid grid-cols-3 items-center gap-4">
-            <div className="space-y-1">
-                 <p className="font-medium flex items-center gap-1"><Search className="h-4 w-4" /> Search Order Number</p>
-			    <Input
-				type="text"
-				name="searchOrder"
-				placeholder="Search orders..."
-				value={orderNumber || ""}
-				onChange={(e) => {
-					const searchParams = new URLSearchParams(window.location.search);
-					searchParams.set("searchOrder", e.target.value);
-					window.history.replaceState(
-						{},
-						document.title,
-						`?${searchParams.toString()}`,
-					);
-				}}
-			    />
-            </div>
-            <div className="space-y-1">
-            <p className="font-medium">Amount Recd</p>
-			<Input
-				type="text"
-				name="amountRecd"
-				placeholder="Amount Recd"
-				disabled
-			/>
-            </div>
-            <div className="space-y-1">
-            <p className="font-medium">Pending Payment</p>
-			<Input
-				type="text"
-				name="pendingPayment"
-				placeholder="Pending Payment"
-				disabled
-			/>
-            </div>
+		<div className="flex flex-wrap items-center justify-between gap-4">
+			<div className="space-y-1">
+				<p className="font-medium flex items-center gap-1">
+					Search Order Number
+				</p>
+				<div className="flex items-center gap-2">
+					<Input
+						type="text"
+						name="searchOrder"
+						placeholder="Search orders..."
+						value={orderNumber || ""}
+						onChange={(e) => {
+							const searchParams = new URLSearchParams(window.location.search);
+							searchParams.set("searchOrder", e.target.value);
+							window.history.replaceState(
+								{},
+								document.title,
+								`?${searchParams.toString()}`,
+							);
+						}}
+					/>
+					<Button>
+						<Search className="h-4 w-4" />
+					</Button>
+				</div>
+			</div>
+			<div className="flex flex-wrap items-center gap-4">
+				<div className="space-y-1">
+					<p className="font-medium">Amount Recd</p>
+					<Input
+						type="text"
+						name="amountRecd"
+						placeholder="Amount Recd"
+						disabled
+					/>
+				</div>
+				<div className="space-y-1">
+					<p className="font-medium">Pending Payment</p>
+					<Input
+						type="text"
+						name="pendingPayment"
+						placeholder="Pending Payment"
+						disabled
+					/>
+				</div>
+			</div>
 		</div>
 	);
 };
